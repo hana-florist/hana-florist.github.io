90119b4382f63b2a0f128824038d0b712568c041
cheeze2000 <me@cheeze2000.net>
2021-12-11 21:11:15 +0800

    feat: default page layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script setup>
+import BottomNavbar from "~/components/BottomNavbar.vue";
+import TopNavbar from "~/components/TopNavbar.vue";
+</script>
+
 <template>
+	<TopNavbar />
 	<router-view />
+	<BottomNavbar />
 </template>
