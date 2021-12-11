64456a058fcc1ff925612fc33a9469b85fa43345
cheeze2000 <cheeze2000@users.noreply.github.com>
2021-12-11 13:14:57 +0000

    deploy: fbf61385cc1e2165d6ce3dff542d88f9eedee297

---
+++ assets/catalogue.006a3892.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.44ba7c36.js";import{o as c,c as a}from"./vendor.23cfbdad.js";const o={};function r(t,n){return c(),a("h1",null,"catalogue")}var f=e(o,[["render",r]]);export{f as default};
