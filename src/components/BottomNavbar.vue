089238717406682ea99dae4641df18336110296c
cheeze2000 <me@cheeze2000.net>
2021-12-11 21:10:49 +0800

    feat: bottom navbar for mobile view

---
+++ src/components/BottomNavbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {
+	BookOpenIcon,
+	HomeIcon,
+	PhoneIcon,
+	ShoppingCartIcon
+} from "@heroicons/vue/outline";
+
+import BottomNavbarItem from "~/components/BottomNavbarItem.vue";
+</script>
+
+<template>
+	<div class="md:hidden fixed bottom-0 px-3 pb-3 w-full">
+		<div class="flex justify-evenly bg-red-900 rounded">
+			<BottomNavbarItem href="/" text="Home">
+				<HomeIcon class="mx-auto w-6 h-6" />
+			</BottomNavbarItem>
+			<BottomNavbarItem href="/catalogue" text="Catalogue">
+				<BookOpenIcon class="mx-auto w-6 h-6" />
+			</BottomNavbarItem>
+			<BottomNavbarItem href="/cart" text="Cart">
+				<ShoppingCartIcon class="mx-auto w-6 h-6" />
+			</BottomNavbarItem>
+			<BottomNavbarItem href="/contact" text="Contact">
+				<PhoneIcon class="mx-auto w-6 h-6" />
+			</BottomNavbarItem>
+		</div>
+	</div>
+</template>
---
+++ src/components/BottomNavbarItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+	href: String,
+	text: String
+});
+</script>
+
+<template>
+	<div class="pt-2 pb-1.5">
+		<router-link :to="href" class="text-red-200">
+			<slot></slot>
+			<p class="mt-1.5 text-xs">
+				{{ text }}
+			</p>
+		</router-link>
+	</div>
+</template>
+
+<style scoped>
+.router-link-active {
+	@apply text-red-400;
+}
+</style>
