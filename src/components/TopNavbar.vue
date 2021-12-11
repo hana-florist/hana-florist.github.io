32e99e0d2dbdd01f3f712333da829cab47fa8106
cheeze2000 <me@cheeze2000.net>
2021-12-11 21:10:58 +0800

    feat: top navbar

---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,35 @@
+<script setup>
+import TopNavbarItem from "~/components/TopNavbarItem.vue";
+</script>
+
+<template>
+	<div class="md:hidden w-full">
+		<div class="flex justify-center py-3 mx-auto">
+			<img
+				class="w-24"
+				src="~~/assets/img/logo.svg"
+				alt="hana floristry logo"
+			/>
+		</div>
+	</div>
+
+	<div class="hidden md:block w-full">
+		<div class="container flex justify-between items-center px-6 py-9 mx-auto">
+			<div>
+				<img
+					class="w-28"
+					src="~~/assets/img/logo.svg"
+					alt="hana floristry logo"
+				/>
+			</div>
+			<div class="flex justify-between">
+				<TopNavbarItem href="/" text="Home" />
+				<TopNavbarItem href="/catalogue" text="Catalogue" />
+				<TopNavbarItem href="/contact" text="Contact" />
+			</div>
+			<div>
+				<TopNavbarItem href="/cart" text="Cart" />
+			</div>
+		</div>
+	</div>
+</template>
---
+++ src/components/TopNavbarItem.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+	href: String,
+	text: String
+});
+</script>
+
+<template>
+	<div class="mx-6">
+		<router-link :to="href">
+			<span class="text-lg text-red-800">
+				{{ text }}
+			</span>
+		</router-link>
+	</div>
+</template>
