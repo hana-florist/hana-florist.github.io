4998a8b2f5021d928a1ae7883e7e768931f26385
cheeze2000 <me@cheeze2000.net>
2021-12-11 09:13:44 +0800

    chore: extend tailwind default colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
 	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				hf: {
+					beige: "rgb(246, 240, 225)",
+					gray: "rgb(81, 78, 69)"
+				}
+			}
+		}
 	},
 	variants: {
 		extend: {}
