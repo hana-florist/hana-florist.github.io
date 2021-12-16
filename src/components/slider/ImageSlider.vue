9b59671ffbd0bbba32af012a5b3889261dee5db3
cheeze2000 <me@cheeze2000.net>
2021-12-16 22:07:01 +0800

    feat: image slider

---
+++ src/components/slider/ImageSlider.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+
+defineProps({
+	images: Array,
+	imgClass: Array
+});
+
+const options = {
+	type: "loop",
+	gap: "3rem",
+	arrows: false
+};
+</script>
+
+<template>
+	<Splide :options="options">
+		<SplideSlide v-for="i in images" :key="i">
+			<img
+				:class="imgClass"
+				:src="i"
+			/>
+		</SplideSlide>
+	</Splide>
+</template>
