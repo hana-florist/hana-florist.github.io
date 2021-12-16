7c5b378e607658efac249139a63cc3f274abc2a1
cheeze2000 <me@cheeze2000.net>
2021-12-16 22:17:32 +0800

    feat: whatsapp shortcut

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@ import "@splidejs/splide/dist/css/splide.min.css";
 
 import BottomNavbar from "~/components/navbar/BottomNavbar.vue";
 import TopNavbar from "~/components/navbar/TopNavbar.vue";
+import WhatsApp from "~/components/misc/WhatsApp.vue";
 </script>
 
 <template>
 	<TopNavbar />
 	<router-view />
 	<BottomNavbar />
+	<WhatsApp />
 </template>
---
+++ src/components/misc/WhatsApp.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="hidden md:block fixed bottom-6 right-6">
+		<img class="w-12" src="~~/assets/img/wa-logo.svg" />
+	</div>
+</template>
