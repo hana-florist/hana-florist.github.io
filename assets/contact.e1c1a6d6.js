785428f7d69957659cb2f10b5dc7c897d8c682e6
cheeze2000 <cheeze2000@users.noreply.github.com>
2021-12-29 14:56:21 +0000

    deploy: 36a95b1ec089f7eccf63e8b47ded1235ba7d0aaf

---
+++ assets/contact.e1c1a6d6.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.d876ee68.js";import{c,o}from"./vendor.b50c3e78.js";const r={};function t(n,a){return o(),c("h1",null,"contact")}var f=e(r,[["render",t]]);export{f as default};
