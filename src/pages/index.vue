c5d4122db94d41faf242cd06def183ff240439df
cheeze2000 <me@cheeze2000.net>
2021-12-29 21:12:31 +0800

    feat: add component to homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
 <script setup>
+import Insta from "~/components/misc/Instagram.vue";
 import MainHero from "~/components/hero/MainHero.vue";
 </script>
 
 <template>
 	<MainHero />
+	<Insta />
 </template>
