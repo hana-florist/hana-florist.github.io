89a08d85d84084dbcad785261e17fa7e9636c7ee
cheeze2000 <me@cheeze2000.net>
2021-12-31 23:27:05 +0800

    feat: location component

---
+++ src/components/misc/Location.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="container block md:flex gap-6 px-3 mx-auto lg:max-w-screen-lg">
+		<div class="mb-4 w-full lg:w-1/2 text-lg text-red-800">
+			<p class="font-cormorant text-3xl">
+				📍Find Us
+			</p>
+			<p class="p-3 pb-0 font-linotte">
+				HANA Floristry
+			</p>
+			<p class="p-3 pt-0 font-lexend">
+				Jl. Boulevard Timur No.28d
+				<br>
+				Komplek Cemara Asri
+				<br>
+				Sumatera Utara 20371, Indonesia
+			</p>
+			<p class="p-3 pb-0 font-linotte">
+				Contact
+			</p>
+			<p class="p-3 pt-0 font-lexend">
+				contact@hana.florist
+				<br>
+				08153898399
+			</p>
+			<p class="p-3 pb-0 font-linotte">
+				Operating Hours
+			</p>
+			<p class="p-3 pt-0 font-lexend">
+				Mon - Sat
+				<br>
+				08:30 - 17:00
+			</p>
+		</div>
+		<iframe
+			class="w-full sm:max-w-full h-80"
+			loading="lazy"
+			src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15927.192225871584!2d98.7045368!3d3.6335252!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5243c0b93ddcbd4a!2sHANA%20Floristry!5e0!3m2!1sen!2ssg!4v1640790795083!5m2!1sen!2ssg"
+		/>
+	</div>
+</template>
