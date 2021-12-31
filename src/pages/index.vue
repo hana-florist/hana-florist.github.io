9f29736483c11449447dbc3fa9cfefb8ed471dbf
cheeze2000 <me@cheeze2000.net>
2022-01-01 00:25:53 +0800

    feat: add location component to homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Insta from "~/components/misc/Instagram.vue";
+import Location from "~/components/misc/Location.vue";
 import MainHero from "~/components/hero/MainHero.vue";
 </script>
 
 <template>
 	<MainHero />
 	<Insta />
+	<Location />
 </template>
