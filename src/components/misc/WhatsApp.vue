1a870524e6df8451f1cb442baaa8e2be75ce4cd3
cheeze2000 <me@cheeze2000.net>
2022-01-01 12:07:43 +0800

    fix: redirect whatsapp shortcut

--- src/components/misc/WhatsApp.vue
+++ src/components/misc/WhatsApp.vue
@@ -1,9 +1,15 @@
 <template>
-	<div class="hidden md:block fixed bottom-6 right-6">
-		<img
-			alt="whatsapp icon"
-			class="w-12"
-			src="~~/assets/img/wa-logo.svg"
-		/>
+	<div class="hidden md:block fixed bottom-3 right-3">
+		<a
+			href="https://api.whatsapp.com/send?phone=628153898399"
+			rel="noreferrer"
+			target="_blank"
+		>
+			<img
+				alt="whatsapp icon"
+				class="w-12"
+				src="~~/assets/img/wa-logo.svg"
+			/>
+		</a>
 	</div>
 </template>
