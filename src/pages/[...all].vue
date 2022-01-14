15823cd108bd3de18e6ef55b75381fe21f5ed464
cheeze2000 <me@cheeze2000.net>
2022-01-14 23:01:37 +0800

    feat: 404 page to redirect to homepage

---
+++ src/pages/[...all].vue
@@ -0,0 +1,23 @@
+<template>
+	<h1 class="my-12 text-3xl text-center">
+		Redirecting...
+	</h1>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+export default {
+	setup() {
+		function redirect() {
+			window.location.href = "/";
+		}
+
+		onMounted(redirect);
+
+		return {
+			redirect
+		};
+	}
+};
+</script>
