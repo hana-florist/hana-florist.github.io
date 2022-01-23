b258925f83ca14c99bea56e36e86d5b6d0225a3a
cheeze2000 <me@cheeze2000.net>
2022-01-23 12:56:52 +0800

    refactor: use script setup syntax sugar

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,23 +1,11 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => window.location.href = "/");
+</script>
+
 <template>
 	<h1 class="my-12 text-3xl text-center">
 		Redirecting...
 	</h1>
 </template>
-
-<script>
-import { onMounted } from "vue";
-
-export default {
-	setup() {
-		function redirect() {
-			window.location.href = "/";
-		}
-
-		onMounted(redirect);
-
-		return {
-			redirect
-		};
-	}
-};
-</script>
