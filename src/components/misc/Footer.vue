ccc2291e3163692feefe038b37917d3e9e4d9d64
cheeze2000 <me@cheeze2000.net>
2022-01-24 01:01:34 +0800

    refactor: increase footer padding

--- src/components/misc/Footer.vue
+++ src/components/misc/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="container pt-12 pb-32 px-3 mx-auto text-red-800">
+	<div class="container pt-20 pb-36 px-3 mx-auto text-red-800">
 		<p class="text-xl">
 			© 2022 HANA Floristry. All rights reserved.
 		</p>
