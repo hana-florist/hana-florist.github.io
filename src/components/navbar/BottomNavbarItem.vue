1554f52f23a63a7c799dfe2efccc38872279b1c3
cheeze2000 <me@cheeze2000.net>
2022-01-24 00:52:35 +0800

    refactor: make navbar item accept external links

--- src/components/navbar/BottomNavbarItem.vue
+++ src/components/navbar/BottomNavbarItem.vue
@@ -1,12 +1,17 @@
 <script setup>
 defineProps({
+	external: Boolean,
 	href: String,
 	text: String
 });
 </script>
 
 <template>
-	<router-link class="basis-1/3 text-red-200" :to="href">
+	<router-link
+		class="basis-1/3 text-red-200"
+		:to="href"
+		v-if="!external"
+	>
 		<div class="pt-2 pb-1.5">
 			<slot></slot>
 			<p class="mt-1.5 text-sm text-center">
@@ -14,6 +19,18 @@ defineProps({
 			</p>
 		</div>
 	</router-link>
+	<a
+		class="basis-1/3 text-red-200"
+		:href="href"
+		v-else
+	>
+		<div class="pt-2 pb-1.5">
+			<slot></slot>
+			<p class="mt-1.5 text-sm text-center">
+				{{ text }}
+			</p>
+		</div>
+	</a>
 </template>
 
 <style scoped>
