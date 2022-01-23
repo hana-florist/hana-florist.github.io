c1617290e917ab5202aaae1ca9464b4c3881ccd4
cheeze2000 <me@cheeze2000.net>
2022-01-24 01:08:45 +0800

    fix: text too large on small screen

--- src/components/misc/CatalogueItem.vue
+++ src/components/misc/CatalogueItem.vue
@@ -14,7 +14,7 @@ defineProps({
 			class="mb-3 aspect-square"
 			:src="src"
 		/>
-		<p class="text-4xl">{{ name }}</p>
+		<p class="text-2xl md:text-4xl">{{ name }}</p>
 		<p class="font-lexend text-base">Rp {{ price }}</p>
 		<p class="font-lexend text-base" v-if="description">({{ description }})</p>
 	</div>
