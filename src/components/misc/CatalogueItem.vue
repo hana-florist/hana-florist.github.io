2c850365895210d404986be7d574173e8c7fbd4e
cheeze2000 <me@cheeze2000.net>
2022-01-24 00:29:56 +0800

    feat: barebones catalogue item

---
+++ src/components/misc/CatalogueItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+	src: String,
+	name: String,
+	price: String,
+	description: String
+});
+</script>
+
+<template>
+	<div class="flex flex-col text-red-800">
+		<img
+			:alt="name"
+			class="mb-3 aspect-square"
+			:src="src"
+		/>
+		<p class="text-4xl">{{ name }}</p>
+		<p class="font-lexend text-base">Rp {{ price }}</p>
+		<p class="font-lexend text-base" v-if="description">({{ description }})</p>
+	</div>
+</template>
